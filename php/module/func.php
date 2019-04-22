341d563bf031ee1469c325192345b5372f60ff72
twinc1e <nadusha_28_97@mail.ru>
2019-04-22 20:34:41 +0400

    export func

--- php/module/func.php
+++ php/module/func.php
@@ -1,5 +1,5 @@
 <?php
-	function sec2hms ($sec, $padHours = false) 
+	function sec2hms ($sec, $padHours = false)
 	  {
 
 	    // start with a blank string
@@ -8,24 +8,24 @@
 	    // do the hours first: there are 3600 seconds in an hour, so if we divide
 	    // the total number of seconds by 3600 and throw away the remainder, we're
 	    // left with the number of hours in those seconds
-	    $hours = intval(intval($sec) / 3600); 
+	    $hours = intval(intval($sec) / 3600);
 
 	    // add hours to $hms (with a leading 0 if asked for)
-	    $hms .= ($padHours) 
+	    $hms .= ($padHours)
 	          ? str_pad($hours, 2, "0", STR_PAD_LEFT). ":"
 	          : $hours. ":";
 
 	    // dividing the total seconds by 60 will give us the number of minutes
 	    // in total, but we're interested in *minutes past the hour* and to get
 	    // this, we have to divide by 60 again and then use the remainder
-	    $minutes = intval(($sec / 60) % 60); 
+	    $minutes = intval(($sec / 60) % 60);
 
 	    // add minutes to $hms (with a leading 0 if needed)
 	    $hms .= str_pad($minutes, 2, "0", STR_PAD_LEFT). ":";
 
 	    // seconds past the minute are found by dividing the total number of seconds
 	    // by 60 and using the remainder
-	    $seconds = intval($sec % 60); 
+	    $seconds = intval($sec % 60);
 
 	    // add seconds to $hms (with a leading 0 if needed)
 	    $hms .= str_pad($seconds, 2, "0", STR_PAD_LEFT);
@@ -34,4 +34,13 @@
 	    return $hms;
 
 	  }
+
+		function export()
+		{
+			//стандартный заголовок, которого обычно хватает
+			header('Content-Type: application/vnd.ms-excel; charset=utf-8');
+			header("Content-Disposition: attachment;filename=sale_receipt.pdf");
+			header("Content-Transfer-Encoding: binary ");
+
+		}
 ?>
